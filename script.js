f12f6b27e9141ba800f794a23ca5b0d969883fb5
Khadijah Aljufri <148049415+KhadijahAljufri@users.noreply.github.com>
2023-11-04 14:21:14 +0700

    Update script.js
    Select the number, and operator items, also the .result div, and equal operator

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const resultInput = document.querySelector('.container .result');
+const numberItems = document.querySelectorAll('.container .items-container .bottom-side .numbers-container div');
+const operatorItems = document.querySelectorAll('.container .items-container .operators-container div');
+const equalOperator = document.querySelector('.container .items-container .bottom-side .equal');
