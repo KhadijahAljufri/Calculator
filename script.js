6713ecd769bd6f73959ba7275c9e339ac0d07a95
Khadijah Aljufri <148049415+KhadijahAljufri@users.noreply.github.com>
2023-11-05 15:19:26 +0700

    Update script.js
    Append the number to the .result div when clicked

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const resultInput = document.querySelector('.container .result');
 const numberItems = document.querySelectorAll('.container .items-container .bottom-side .numbers-container div');
 const operatorItems = document.querySelectorAll('.container .items-container .operators-container div');
 const equalOperator = document.querySelector('.container .items-container .bottom-side .equal');
+
+numberItems.forEach(item => {
+    item.addEventListener('click', () => {
+        resultInput.append(item.textContent)
+    })
+});
+operatorItems.forEach(item => {
+    item.addEventListener('click', () => {
+        let itemImage = document.createElement("img")
+        itemImage.src = `./assets/${item.className}.svg`
+        resultInput.append(itemImage)
+    })
+});
